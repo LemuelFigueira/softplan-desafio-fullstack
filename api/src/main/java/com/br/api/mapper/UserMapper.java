cabcf9c3a75d8af69a31b8db5cdcb7507d7d248d
Lemuel Figueira <lemuel.figueira@iksolution.com.br>
2022-05-23 21:23:45 -0300

    feat: UserPageableResponseDTO
    dto para busca de usuário paginada
    funcao para converter um map para o dto no user mapper

--- api/src/main/java/com/br/api/mapper/UserMapper.java
+++ api/src/main/java/com/br/api/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package com.br.api.mapper;
 
+import java.util.Map;
+
 import com.br.api.dto.request.UserRequestDTO;
+import com.br.api.dto.response.UserPageableResponseDTO;
 import com.br.api.dto.response.UserResponseDTO;
 import com.br.api.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class UserMapper {
 
@@ -19,5 +23,11 @@ public final class UserMapper {
                 .name(user.getName())
                 .email(user.getEmail())
                 .profile(user.getProfile()).build();
+    }
+
+    public static UserPageableResponseDTO pageableMapToResonse(Map<String, Object> map) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.convertValue(map, UserPageableResponseDTO.class);
     }
 }
