6a12017d06accff832e91634187bb2fc67885c3a
Lemuel Figueira <lemuel.figueira@iksolution.com.br>
2022-05-23 21:24:29 -0300

    feat: user repository
    busca paginada de usuarios contendo nome ou perfil ou email

--- api/src/main/java/com/br/api/repository/UserRepository.java
+++ api/src/main/java/com/br/api/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.br.api.model.User;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +21,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     public Page<User> findAll(Pageable pageable);
+
+    @Query(value = "select u from User u where u.name like %?1% or u.email like %?1% or u.profile like %?1%")
+    public Page<User> findByAnyNameOrProfileOrEmail(@Param("query") String query, Pageable pageable);
 }
